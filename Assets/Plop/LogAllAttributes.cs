6122169405ca78e61a50b9a01fd657232ae15805
belka_lif <belkacem.lahouel@gmail.com>
2016-01-28 18:34:26 -0500

    Adding Attrbitues logger. TODO: get value of attributes

---
+++ Assets/Plop/LogAllAttributes.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.IO;
+using System.Text;
+using System;
+using System.Reflection;
+
+public class LogAllAttributes : MonoBehaviour
+{
+    private static readonly string DIR = "./PlopLogs/";
+    private static readonly string FILE = "plop.log";
+    private FindAllGameObjects finder;
+
+    public void Start()
+    {
+        finder = GameObject.FindGameObjectWithTag("Plop").GetComponent<FindAllGameObjects>();
+    }
+
+    public void Update()
+    {
+        if (!Input.GetKeyDown(KeyCode.KeypadEnter))
+            return;
+
+        StringBuilder text = new StringBuilder();
+        foreach (GameObject go in finder.getCurrentMonoBehaviorObjects())
+        {
+            text.AppendLine(go.ToString());
+            Component[] components = go.GetComponents(typeof(MonoBehaviour));
+            foreach (Component c in components)
+            {
+                PropertyInfo[] properties = GetType().GetProperties();
+                foreach (PropertyInfo p in properties)
+                {
+                    text.Append("\t" + p.ToString());
+                    text.AppendLine(": value (TODO)");
+                }
+            }
+            
+        }
+        File.WriteAllText(DIR + myNowToString() + "_" + FILE, text.ToString());
+    }
+
+    private string myNowToString()
+    {
+        return DateTime.Now.Year.ToString() + "-" +
+                DateTime.Now.Month.ToString() + "-" +
+                DateTime.Now.Day.ToString() + "_" +
+                DateTime.Now.Hour.ToString() + "-" +
+                DateTime.Now.Minute.ToString() + "-" +
+                DateTime.Now.Second.ToString();
+    }
+}
