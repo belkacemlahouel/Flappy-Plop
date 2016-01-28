58ebf14f45c7b7076c48dba66a41f2a8898fac1c
belka_lif <belkacem.lahouel@gmail.com>
2016-01-28 17:32:50 -0500

    Finding all MonoBehavior GameObjects

---
+++ Assets/Plop/FindAllGameObjects.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class FindAllGameObjects : MonoBehaviour
+{
+
+    private LinkedList<GameObject> currentMonoBehaviorObjects;
+    private int number;
+
+	public void Start()
+    {
+        currentMonoBehaviorObjects = new LinkedList<GameObject>();
+        number = 0;
+    }
+
+    public void Update()
+    {
+        currentMonoBehaviorObjects.Clear();
+        number = 0;
+
+        GameObject[] allObjects = UnityEngine.Object.FindObjectsOfType<GameObject>();
+        foreach (GameObject go in allObjects)
+        {
+            if (go != gameObject && go.activeInHierarchy && go.GetComponent<MonoBehaviour>() != null)
+            {
+                currentMonoBehaviorObjects.AddLast(go);
+                ++number;
+            }
+        }
+    }
+
+    public LinkedList<GameObject> getCurrentMonoBehaviorObjects()
+    {
+        //Computation is done inside Update to make sure computation is not too heavy
+        return currentMonoBehaviorObjects;
+    }
+
+    public int getNumberOfCurrentMonoBehaviorObjects()
+    {
+        return number;
+    }
+}
---
+++ Assets/Plop/TestFindAllGameObjects.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestFindAllGameObjects : MonoBehaviour
+{
+    private FindAllGameObjects finder;
+
+	public void Start()
+    {
+        finder = GameObject.FindGameObjectWithTag("Plop").GetComponent<FindAllGameObjects>();
+    }
+
+    public void Update()
+    {
+        Debug.Log(finder.getNumberOfCurrentMonoBehaviorObjects());
+        if (Input.GetKeyDown(KeyCode.KeypadEnter))
+        {
+            foreach (GameObject go in finder.getCurrentMonoBehaviorObjects())
+            {
+                Debug.Log(go);
+            }
+        }
+    }
+}
